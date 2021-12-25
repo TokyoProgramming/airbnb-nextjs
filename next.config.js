fe4be531dc9ec2958330861a6605c72eafb823b3
TokyoProgramming <samuraidjango.jp@gmail.com>
2021-12-25 10:31:09 +0900

    add map

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@ module.exports = {
   images: {
     domains: ['links.papareact.com'],
   },
+
+  env: {
+    mapbox_key:
+      'pk.eyJ1IjoidG9reW9wcm9ncmFtbWluZyIsImEiOiJja3hsMnY4eTAydjQzMzBwem5yZGU2ZTAxIn0.OwYvp1KJ8hqDHhblm28CGA',
+  },
 };
